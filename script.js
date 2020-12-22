fdae28782092c121309d8d2754692a8f9e7c4338
Jennifer Huynh <jntlhuynh@gmail.com>
2020-12-21 23:58:38 -0800

    set up the orientation of the calculator

--- script.js
+++ script.js
@@ -9,20 +9,21 @@ function operate (operator, first, second) {
         case "*":
             return first * second;
         case "/":
-            return (second === 0) ? 'Nah. That ain\'t it, chief' : first / second;
+            return (second === 0) ? 'Nah. That ain\'t it, chief. Try again!' : first / second;
         default :
             throw 'Error: no operator selected';
     }
 }
 
 function setupDivs () {
-    const display = document.querySelector('.display-area');
-    const clear = document.querySelector('.clear-divs');
-    const operations = document.querySelector('.operations-divs');
-    const numbers = document.querySelector('.numbers-divs');
+    // const display = document.querySelector('.display-area');
+    // const clear = document.querySelector('.clear-divs');
+    // const operations = document.querySelector('.operations-divs');
+    // const numbers = document.querySelector('.numbers-divs');
     // TODO: Set up the calculator grids 
     // using the grid lines
     
 }
+// TODO: Add factorial and exponent
 
 setupDivs();
