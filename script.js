436816602c4aed7a826bfe83e615dd5f84991ca5
Jennifer Huynh <jntlhuynh@gmail.com>
2021-01-06 17:26:47 -0800

    implemented the basics of the operate function

--- script.js
+++ script.js
@@ -1,35 +1,47 @@
 const container = document.querySelector('.calculator-keys'); 
 let initCalcVal = false;
+let expression = {firstVal: null, secVal: null, operation: null};
 let displayVal = "";
 
-function Expression (operator, first, second) {
-    this.operator = operator;
-    this.first = first;
-    this.second = second;
-}
 function factorial (n) {
     return (n == 0) ? 1 : n * factorial(n - 1);
 }
 
-function operate (operator, first, second) {
-    switch(operator) {
+function clearDispVal() {
+    displayVal = "";
+}
+
+function clearExpr() {
+    expression.firstVal = null;
+    expression.secVal = null;
+    expression.operation = null;
+}
+
+function updateDisp(displayVal) {
+    console.log(displayVal.toString());
+    const inputField = document.querySelector("#input-field");
+    inputField.innerHTML = displayVal.toString();
+}
+
+function operate (expression) {
+    switch(expression.operation) {
         case "+":
-            displayVal = first + second;
+            displayVal = expression.firstVal + expression.secVal;
             break;
         case "-":
-            displayVal = first - second;
+            displayVal = expression.firstVal - expression.secVal;
             break;
         case "*":
-            displayVal = first * second;
+            displayVal = expression.firstVal * expression.secVal;
             break;
         case "/":
-            (second === 0) ? displayVal = 'Nah. That ain\'t it, chief. Try again!' : displayVal = first / second;
+            (expression.secVal === 0) ? displayVal = 'Nah. That ain\'t it, chief. Try again!' : displayVal = expression.firstVal / expression.secVal;
             break;
         case "!":
-            displayVal = factorial(n);
+            displayVal = factorial(expression.firstVal);
             break;
         case "x":
-            displayVal = Math.pow(first, second);
+            displayVal = Math.pow(expression.firstVal, expression.secVal);
             break;
         default:
             throw 'Error: operate method failed';
@@ -42,40 +54,99 @@ function showNumDisplay(e) {
         inputField.innerHTML = "";
         initCalcVal = false;
     }
+    if (displayVal === "0") {
+        clearDispVal();
+    }
     displayVal += e.target.id;
     inputField.innerHTML += e.target.id;
 }
 
+function getSecVal(exp) {
+    if (exp.firstVal != null && exp.operation != null) {
+        exp.secVal = Number(displayVal);
+        operate(exp);
+        updateDisp(displayVal);
+        expression.operation = null;
+    } 
+}
+
+function getFirstVal(exp, op) {
+    exp.firstVal = Number(displayVal);
+    exp.operation = op;
+    clearDispVal();
+}
+
 function execOp(e) {
+    const inputField = document.querySelector("#input-field");
+    // console.log(e.target.id);
     switch(e.target.id) {
+        case "delete":
+            if (displayVal === "") {
+                updateDisp(0);
+            } else {
+                displayVal = displayVal.slice(0,-1);
+                updateDisp(displayVal);
+            }
+            break;
+
         case "AC":
-            displayVal = "";
+            clearDispVal();
             inputField.innerHTML = "0";
+            clearExpr();
             break;
+
         case "exponent":
+            getSecVal(expression);
+            getFirstVal(expression, "x");
             break;
+
         case "factorial":
+            getFirstVal(expression, "!");
+            operate(expression);
             break;
+
         case "divide":
+            getSecVal(expression);
+            getFirstVal(expression, "/");
             break;
         case "times":
+            getSecVal(expression);
+            getFirstVal(expression, "*");
             break;
+
         case "plus":
+            getSecVal(expression);
+            getFirstVal(expression, "+");
             break;
+
         case "minus":
+            getSecVal(expression);
+            getFirstVal(expression, "-");
             break;
+
         case "sign":
+            (displayVal > 0) ? displayVal = "-" + displayVal : displayVal = displayVal.slice(1, displayVal.length);
+            updateDisp(displayVal);
             break;
+
         case "decimal":
             break;
+
         case "equal":
+            if (expression.operation == null) {
+                clearDispVal();
+                updateDisp(0);
+                clearExpr();
+            }
+            getSecVal(expression);
+            
             break;
+
         default:
             throw 'Error: execOp failed';
     }
 }
 function setupDivs () {
-    // let expression = new Expression(null, null, null);
     const numbers = document.querySelectorAll(".digit");
     numbers.forEach(number => number.addEventListener('click', showNumDisplay));
     initCalcVal = true;
@@ -84,5 +155,4 @@ function setupDivs () {
     operators.forEach(operator => operator.addEventListener('click', execOp));
 }
 // TODO: Add factorial and exponent
-
 setupDivs();
